e3acef369232e5defe5782b5071aa2856de2a3a5
Jun Ke <jun.ke@wirelesscar.com>
2024-08-09 11:32:29 +0800

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sqlitelib",
-    version="1.0.1",
+    version="1.0.2",
     author="Jun Ke",
     author_email="kejun91@gmail.com",
     description="A sqlite lib that encapsulate common sqlite operations",
